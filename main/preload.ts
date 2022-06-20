dcd59a2cf825fe821b1f449127a7e20f8b71a819
KwonJin Jung <91672778+qwp0905@users.noreply.github.com>
2022-06-20 12:15:05 +0900

    Update preload.ts

--- main/preload.ts
+++ main/preload.ts
@@ -1,1 +1,12 @@
-export {}
+const { contextBridge, ipcRenderer } = require("electron");
+
+contextBridge.exposeInMainWorld(
+  'api',{
+    request:(channel,data)=>{
+      ipcRenderer.send(channel,data)
+      ipcRenderer.on('reply',(event,arg)=>{
+        console.log(arg)
+      })
+    }
+  }
+)
